8ec8571e9274dbbc45c193719672df21aa46b8f1
Fredrik Skatland <fredrikskatland@gmail.com>
2024-06-29 12:09:53 +0200

    fix login form for email

--- code/frontend/kometa/src/components/Login.vue
+++ code/frontend/kometa/src/components/Login.vue
@@ -39,7 +39,7 @@
         <div class="flex justify-center w-full mb-3">
           <div class="flex items-center">
           <button 
-          class="mr-3 font-semibold text-gray-700 text-center"
+          class="mr-3 font-semibold text-gray-700 text-center border border-gray-300 rounded-md shadow-md px-3 py-3"
           @click="handleGoogleLogin"
           role="button"
           tabindex="0"
@@ -58,27 +58,53 @@
           <hr class="flex-1 border-gray-300" />
         </div>
         <div class="flex justify-center w-full mb-3">
+          <div class="flex items-center">
+            <!-- Input form email, password and confirm password -->
+            <form @submit.prevent="createUser">
+                <div class="mb-1 sm:mb-2">
+                  <label for="email" class="inline-block mb-1 font-medium">E-mail</label>
+                  <input
+                    v-model="username"
+                    placeholder="john.doe@example.org"
+                    required
+                    type="text"
+                    class="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                    id="email"
+                    name="email"
+                  />
+                </div>
+                <div class="mb-1 sm:mb-2">
+                  <label for="password" class="inline-block mb-1 font-medium">Password</label>
+                  <input
+                    v-model="password"
+                    placeholder=""
+                    required
+                    type="password"
+                    class="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                    id="password"
+                    name="password"
+                  />
+                </div>
+                <div class="mt-4 mb-2 sm:mb-4">
+                  <button 
+                    type="submit"
+                    @click="doLogin"
+                    class="mr-3 font-semibold text-gray-700 text-center shadow-md px-3 py-3 border border-gray-300 rounded-md"
+                    role="button"
+                    tabindex="0"
+                    @keypress.enter="doLogin"
+                    @keypress.space="doLogin"
+                    >
+                        Log in email 
+                    <font-awesome-icon :icon="['fas', 'envelope']" />
+                </button>
+                </div>
+              </form>
+            </div>
           <a
             href="/"
             class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
           >
-            <div class="flex items-center">
-              <div 
-              class="mr-3 font-semibold text-gray-700 text-center"
-              @click="doLogin"
-              role="button"
-              tabindex="0"
-              @keypress.enter="doLogin"
-              @keypress.space="doLogin"
-              >
-                Login with email
-              </div>
-              <svg viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
-                <path
-                  d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z"
-                ></path>
-              </svg>
-            </div>
           </a>
         </div>
         
